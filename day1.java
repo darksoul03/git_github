1e775b216b6ff9bd90b2a1f4e0b2f568030a2a83
adityafamily0312 <aditya.family0312@gmail.com>
2020-08-10 02:58:47 +0530

    All files added

---
+++ day1.java
@@ -0,0 +1,11 @@
+public class day1{
+
+    public static void main(String[] args){
+
+      int a,b=7,c;
+      a=12;
+      c=a*b+6;
+      System.out.println("Hello: "+(a*b));
+      System.out.print("Aditya");
+      System.out.println("c: "+c);
+}
---
+++ numbers.java
@@ -0,0 +1,11 @@
+public class numbers{
+  public static void main(String[] args) {
+    int age;
+    age=27;
+
+    int a,b=7,c=8,d=22;
+    a=age;
+    System.out.println((2*a+32+4*d+c));
+
+  }
+}
